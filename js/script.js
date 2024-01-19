87e2ce336f454181ddb580c465ca6fe787b027b0
DevJm <116467991+NotJm@users.noreply.github.com>
2024-01-18 20:51:28 -0600

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-export const sum = (x, y) => x + y;
-export const res = (x, y) => x - y;
-export const mul = (x, y) => x * y;
-export const div = (x, y) => {
-    if (y < 0) 
-        return "Operacion no valida";
-    else
-        return x / y;
-}
